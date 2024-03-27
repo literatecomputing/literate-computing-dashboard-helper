bbe94850134b81e558379d7fb3367f1ca4dfabfc
Jay Pfaffman <jay@literatecomputing.com>
2024-03-27 10:21:53 -0500

    add server id

--- javascripts/discourse/connectors/admin-upgrade-header/pfaffmanager-info.js
+++ javascripts/discourse/connectors/admin-upgrade-header/pfaffmanager-info.js
@@ -1,5 +1,12 @@
 import Component from "@glimmer/component";
 export default class AboutTextComponent extends Component {
+  get dashboardUrl() {
+    if (settings.dashboard_server_id > 0) {
+      return `https://dashboard.literatecomputing.com/pfaffmanager/servers/${settings.dashboard_server_id}`;
+    } else {
+      return "https://dashboard.literatecomputing.com";
+    }
+  }
   get haveDashboard() {
     return settings.include_dashboard_link;
   }
