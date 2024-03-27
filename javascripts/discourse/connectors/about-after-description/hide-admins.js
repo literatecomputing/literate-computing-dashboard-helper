1cc1a41b267c1a9d855adee86af7d43011c25c7c
Jay Pfaffman <jay@literatecomputing.com>
2024-03-27 10:04:39 -0500

    more lint

--- javascripts/discourse/connectors/about-after-description/hide-admins.js
+++ javascripts/discourse/connectors/about-after-description/hide-admins.js
@@ -1,15 +1,14 @@
 import Component from "@glimmer/component";
 // import { inject as service } from "@ember/service";
-import { action } from '@ember/object';
+import { action } from "@ember/object";
 
 export default class AboutTextComponent extends Component {
-
   @action
   hideAdmins() {
     const usersArray = settings.hide_admin_users.split("|");
     for (const user of usersArray) {
       const e = document.querySelector(`div[data-username='${user}']`);
-      if (e){
+      if (e) {
         e.classList.add("hidden");
       }
     }
