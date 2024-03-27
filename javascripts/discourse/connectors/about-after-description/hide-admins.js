88955604bb0fde8ad0961562412ad1920980dcb1
Jay Pfaffman <jay@literatecomputing.com>
2024-03-27 09:51:54 -0500

    lint

--- javascripts/discourse/connectors/about-after-description/hide-admins.js
+++ javascripts/discourse/connectors/about-after-description/hide-admins.js
@@ -5,15 +5,12 @@ import { action } from '@ember/object';
 export default class AboutTextComponent extends Component {
 
   @action
-  hideAdmins(element) {
+  hideAdmins() {
     const usersArray = settings.hide_admin_users.split("|");
     for (const user of usersArray) {
       const e = document.querySelector(`div[data-username='${user}']`);
       if (e){
         e.classList.add("hidden");
-      } else {
-        console.log(`did not find ${user}`);
-        continue;
       }
     }
   }
